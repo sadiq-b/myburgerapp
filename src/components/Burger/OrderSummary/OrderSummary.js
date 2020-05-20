72f8311c708088a80bf261d52ec22a5b04032b5b
sadiq-b <babalolasadiq@yahoo.com>
2020-05-20 01:41:59 +0100

    first phase of burgerapp

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+import Aux from '../../../hoc/Auxi/Aux';
+import  Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+
+    componentWillUpdate(){
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render(){
+        const ingredientSummary=Object.keys(this.props.ingredients)
+       .map(igKey => {
+       return <li key={igKey}><span style={{textTransform:'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}</li>
+       });
+        return(<Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul >
+               {ingredientSummary}
+            </ul>
+          <p><strong>TotalPrice:{this.props.price.toFixed(2)}</strong></p>
+            <p>continue to checkout</p>
+             <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button> 
+             <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button> 
+              
+              </Aux>);
+    }
+}
+
+export default OrderSummary;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+ import  React from 'react';
+
+ import  burgerLogo from '../../assets/images/27.1 burger-logo.png.png'
+ import classes from './Logo.css'
+
+const logo=(props)=>(
+  <div className= {classes.Logo}>
+      <img src={burgerLogo} alt="MyBurger"/>
+  </div>
+
+);
+
+export default logo;
